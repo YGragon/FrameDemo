cc68acbc64e87118def03c516b039013f18d53fa
Aller <1105894953@qq.com>
2019-07-28 22:15:19 +0800

    fix bug

---
+++ lib_common/build/tmp/kapt3/stubs/debug/com/example/lib_common/http/UrlConstant.java
@@ -0,0 +1,17 @@
+package com.example.lib_common.http;
+
+import java.lang.System;
+
+/**
+ * * URL
+ */
+@kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\b\u00c6\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0005"}, d2 = {"Lcom/example/lib_common/http/UrlConstant;", "", "()V", "BASE_URL", "", "lib_common_debug"})
+public final class UrlConstant {
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String BASE_URL = "https://www.wanandroid.com/";
+    public static final com.example.lib_common.http.UrlConstant INSTANCE = null;
+    
+    private UrlConstant() {
+        super();
+    }
+}
---
+++ lib_common/build/tmp/kapt3/stubs/debug/com/example/lib_common/model/User.java
@@ -0,0 +1,171 @@
+package com.example.lib_common.model;
+
+import java.lang.System;
+
+/**
+ * * chapterTops : []
+ * * collectIds : []
+ * * email :
+ * * icon :
+ * * id : 20685
+ * * password :
+ * * token :
+ * * type : 0
+ * * username : xing2019
+ */
+@kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000$\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010!\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\b\n\u0002\b\u001f\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0086\b\u0018\u00002\u00020\u0001BY\u0012\f\u0010\u0002\u001a\b\u0012\u0004\u0012\u00020\u00040\u0003\u0012\f\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00060\u0003\u0012\u0006\u0010\u0007\u001a\u00020\u0004\u0012\u0006\u0010\b\u001a\u00020\u0004\u0012\u0006\u0010\t\u001a\u00020\u0006\u0012\u0006\u0010\n\u001a\u00020\u0004\u0012\u0006\u0010\u000b\u001a\u00020\u0004\u0012\u0006\u0010\f\u001a\u00020\u0006\u0012\u0006\u0010\r\u001a\u00020\u0004\u00a2\u0006\u0002\u0010\u000eJ\u000f\u0010\u001b\u001a\b\u0012\u0004\u0012\u00020\u00040\u0003H\u00c6\u0003J\u000f\u0010\u001c\u001a\b\u0012\u0004\u0012\u00020\u00060\u0003H\u00c6\u0003J\t\u0010\u001d\u001a\u00020\u0004H\u00c6\u0003J\t\u0010\u001e\u001a\u00020\u0004H\u00c6\u0003J\t\u0010\u001f\u001a\u00020\u0006H\u00c6\u0003J\t\u0010 \u001a\u00020\u0004H\u00c6\u0003J\t\u0010!\u001a\u00020\u0004H\u00c6\u0003J\t\u0010\"\u001a\u00020\u0006H\u00c6\u0003J\t\u0010#\u001a\u00020\u0004H\u00c6\u0003Jo\u0010$\u001a\u00020\u00002\u000e\b\u0002\u0010\u0002\u001a\b\u0012\u0004\u0012\u00020\u00040\u00032\u000e\b\u0002\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00060\u00032\b\b\u0002\u0010\u0007\u001a\u00020\u00042\b\b\u0002\u0010\b\u001a\u00020\u00042\b\b\u0002\u0010\t\u001a\u00020\u00062\b\b\u0002\u0010\n\u001a\u00020\u00042\b\b\u0002\u0010\u000b\u001a\u00020\u00042\b\b\u0002\u0010\f\u001a\u00020\u00062\b\b\u0002\u0010\r\u001a\u00020\u0004H\u00c6\u0001J\u0013\u0010%\u001a\u00020&2\b\u0010\'\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010(\u001a\u00020\u0006H\u00d6\u0001J\t\u0010)\u001a\u00020\u0004H\u00d6\u0001R\u0017\u0010\u0002\u001a\b\u0012\u0004\u0012\u00020\u00040\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000f\u0010\u0010R\u0017\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00060\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0011\u0010\u0010R\u0011\u0010\u0007\u001a\u00020\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0012\u0010\u0013R\u0011\u0010\b\u001a\u00020\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\u0013R\u0011\u0010\t\u001a\u00020\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0015\u0010\u0016R\u0011\u0010\n\u001a\u00020\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0017\u0010\u0013R\u0011\u0010\u000b\u001a\u00020\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0018\u0010\u0013R\u0011\u0010\f\u001a\u00020\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0019\u0010\u0016R\u0011\u0010\r\u001a\u00020\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u001a\u0010\u0013\u00a8\u0006*"}, d2 = {"Lcom/example/lib_common/model/User;", "", "chapterTops", "", "", "collectIds", "", "email", "icon", "id", "password", "token", "type", "username", "(Ljava/util/List;Ljava/util/List;Ljava/lang/String;Ljava/lang/String;ILjava/lang/String;Ljava/lang/String;ILjava/lang/String;)V", "getChapterTops", "()Ljava/util/List;", "getCollectIds", "getEmail", "()Ljava/lang/String;", "getIcon", "getId", "()I", "getPassword", "getToken", "getType", "getUsername", "component1", "component2", "component3", "component4", "component5", "component6", "component7", "component8", "component9", "copy", "equals", "", "other", "hashCode", "toString", "lib_common_debug"})
+public final class User {
+    @org.jetbrains.annotations.NotNull()
+    private final java.util.List<java.lang.String> chapterTops = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.util.List<java.lang.Integer> collectIds = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String email = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String icon = null;
+    private final int id = 0;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String password = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String token = null;
+    private final int type = 0;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String username = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.lang.String> getChapterTops() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.lang.Integer> getCollectIds() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getEmail() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getIcon() {
+        return null;
+    }
+    
+    public final int getId() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getPassword() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getToken() {
+        return null;
+    }
+    
+    public final int getType() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getUsername() {
+        return null;
+    }
+    
+    public User(@org.jetbrains.annotations.NotNull()
+    java.util.List<java.lang.String> chapterTops, @org.jetbrains.annotations.NotNull()
+    java.util.List<java.lang.Integer> collectIds, @org.jetbrains.annotations.NotNull()
+    java.lang.String email, @org.jetbrains.annotations.NotNull()
+    java.lang.String icon, int id, @org.jetbrains.annotations.NotNull()
+    java.lang.String password, @org.jetbrains.annotations.NotNull()
+    java.lang.String token, int type, @org.jetbrains.annotations.NotNull()
+    java.lang.String username) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.lang.String> component1() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.lang.Integer> component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component3() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component4() {
+        return null;
+    }
+    
+    public final int component5() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component6() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component7() {
+        return null;
+    }
+    
+    public final int component8() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component9() {
+        return null;
+    }
+    
+    /**
+     * * chapterTops : []
+     * * collectIds : []
+     * * email :
+     * * icon :
+     * * id : 20685
+     * * password :
+     * * token :
+     * * type : 0
+     * * username : xing2019
+     */
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.lib_common.model.User copy(@org.jetbrains.annotations.NotNull()
+    java.util.List<java.lang.String> chapterTops, @org.jetbrains.annotations.NotNull()
+    java.util.List<java.lang.Integer> collectIds, @org.jetbrains.annotations.NotNull()
+    java.lang.String email, @org.jetbrains.annotations.NotNull()
+    java.lang.String icon, int id, @org.jetbrains.annotations.NotNull()
+    java.lang.String password, @org.jetbrains.annotations.NotNull()
+    java.lang.String token, int type, @org.jetbrains.annotations.NotNull()
+    java.lang.String username) {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object p0) {
+        return false;
+    }
+}
