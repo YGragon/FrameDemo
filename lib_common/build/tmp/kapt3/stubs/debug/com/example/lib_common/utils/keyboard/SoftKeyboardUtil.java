bdae7d54d56072250ce83a12762537f517946399
Aller <1105894953@qq.com>
2019-12-22 22:49:35 +0800

    add androidjetpack

---
+++ lib_common/build/tmp/kapt3/stubs/debug/com/example/lib_common/utils/keyboard/SoftKeyboardUtil.java
@@ -0,0 +1,27 @@
+package com.example.lib_common.utils.keyboard;
+
+import java.lang.System;
+
+/**
+ * * 软键盘工具类
+ */
+@kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\b\u00c6\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u000e\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0006J\u000e\u0010\u0007\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0006J\u000e\u0010\b\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0006\u00a8\u0006\t"}, d2 = {"Lcom/example/lib_common/utils/keyboard/SoftKeyboardUtil;", "", "()V", "hideKeyboard", "", "view", "Landroid/view/View;", "showKeyboard", "toggleSoftInput", "lib_common_debug"})
+public final class SoftKeyboardUtil {
+    public static final com.example.lib_common.utils.keyboard.SoftKeyboardUtil INSTANCE = null;
+    
+    public final void showKeyboard(@org.jetbrains.annotations.NotNull()
+    android.view.View view) {
+    }
+    
+    public final void hideKeyboard(@org.jetbrains.annotations.NotNull()
+    android.view.View view) {
+    }
+    
+    public final void toggleSoftInput(@org.jetbrains.annotations.NotNull()
+    android.view.View view) {
+    }
+    
+    private SoftKeyboardUtil() {
+        super();
+    }
+}
