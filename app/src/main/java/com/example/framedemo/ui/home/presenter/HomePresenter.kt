912844c1b13c3dc05cf9a7bef51bf97d541022ef
Aller <1105894953@qq.com>
2020-09-30 21:37:16 +0800

    [func] 添加 ExceptionHandle 处理 runRxLambda 返回的异常

--- app/src/main/java/com/example/framedemo/ui/home/presenter/HomePresenter.kt
+++ app/src/main/java/com/example/framedemo/ui/home/presenter/HomePresenter.kt
@@ -6,9 +6,11 @@ import com.example.lib_common.base.BasePresenter
 import com.example.lib_common.db.AppDataBase
 import com.example.lib_common.db.dao.HotKeyDao
 import com.example.lib_common.http.RetrofitManager
+import com.example.lib_common.http.exception.ExceptionHandle
 import com.example.lib_common.http.runRxLambda
 import com.example.lib_common.http.scheduler.SchedulerUtils
 import com.example.lib_common.utils.LogUtils
+import org.xml.sax.ErrorHandler
 
 /**
  * 首页 Presenter
@@ -23,7 +25,9 @@ class HomePresenter : BasePresenter<HomeContract.View>(),HomeContract.Presenter{
             hotkeyDao.getAllHotKey().clear()
             hotkeyDao.insertAll(it.data)
             mRootView?.showHotkeys(it.data)
-        },{},{
+        },{
+            mRootView?.showError(ExceptionHandle.handleException(it))
+        },{
             addSubscription(it)
         })
     }
