72bffd2cef304f68a245ec043660d8a140dd63bb
Aller <1105894953@qq.com>
2020-10-04 22:38:24 +0800

    [fix] 暂时关闭 lifeOwner 接口

--- lib_common/src/main/java/com/example/lib_common/base/IPresenter.kt
+++ lib_common/src/main/java/com/example/lib_common/base/IPresenter.kt
@@ -10,20 +10,20 @@ import org.jetbrains.annotations.NotNull
 /**
  * MVP 基础接口 Presenter
  */
-interface IPresenter<in V: IBaseView>: LifecycleObserver {
+interface IPresenter<in V: IBaseView> {
 
     fun attachView(mRootView: V)
 
     fun detachView()
 
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
-    fun onCreate(@NotNull owner: LifecycleOwner)
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    fun onDestroy(@NotNull owner: LifecycleOwner)
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
-    fun onLifecycleChanged(@NotNull owner: LifecycleOwner, @NotNull event: Lifecycle.Event
-    )
+//    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+//    fun onCreate(@NotNull owner: LifecycleOwner)
+//
+//    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+//    fun onDestroy(@NotNull owner: LifecycleOwner)
+//
+//    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+//    fun onLifecycleChanged(@NotNull owner: LifecycleOwner, @NotNull event: Lifecycle.Event
+//    )
 }
